27565da36d45c0d6032fc6d0daa3e36487f6d1fc
pgap22 <78711175+pgap22@users.noreply.github.com>
2023-07-12 21:20:53 -0600

    pepe el pepito feliz

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Index from './pages/Index'
+
+
+const router = createBrowserRouter([
+  
+  {
+    path: "/",
+    element: <Index />
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 )
