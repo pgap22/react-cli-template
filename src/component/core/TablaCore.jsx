830abcc6ce10de2b4229ae72d57dc7f71c01f69b
pgap22 <78711175+pgap22@users.noreply.github.com>
2023-07-23 15:34:26 -0600

    Update TablaCore.jsx

--- src/component/core/TablaCore.jsx
+++ src/component/core/TablaCore.jsx
@@ -1,19 +1,26 @@
 import TablaCRUD from "src/component/tablaCrud"
 import useApi from "src/hooks/useAPI"
 
+import { Link } from "react-router-dom"
+import Button from "src/component/button"
+
+
 //export default TablaCore() <- add export default on your pages Component
 
 export default function TablaCore(){
 
-    const api = useApi("/your_express_route")
+    const api = useApi("/usuario") // '/your_express_route'
 
     return (
         <>
         {/* You can put more html elements from here ! */}
-        
+        <Link>
+            <Button>Crear Usuario</Button>
+        </Link>
         {/* to here */}
-        
-        <TablaCRUD name={"your_express_route_without_slash"} api={api} />
+
+        {/* name=your_express_route_without_slash  */}
+        <TablaCRUD name={"usuario"} api={api} />
 
         {/* You can put more html elements from here ! */}
         
